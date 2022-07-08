911c8a192567e1064175abdb461de1ced40357ae
andrea <yamacias30@misena.edu.co>
2022-07-08 12:25:10 -0500

    tabla

--- archivo.php
+++ archivo.php
@@ -6,7 +6,25 @@
 
 <?php
 echo "Hello World!";
+
 ?> 
+<table>
+  <tr>
+    <th>nombre</th>
+    <th>apellido</th>
+    <th>pais</th>
+  </tr>
+  <tr>
+    <td>Cindy</td>
+    <td>Hurtado</td>
+    <td>EEUU</td>
+  </tr>
+  <tr>
+    <td>Valeria</td>
+    <td>vargas</td>
+    <td>España</td>
+  </tr>
+</table>
 
 </body>
 </html>
